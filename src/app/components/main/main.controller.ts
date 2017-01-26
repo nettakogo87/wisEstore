d21ee4398ee39a1aa15f6693b17132bdd1d8923d
nettakogo87 <bratsatkan@gmail.com>
2017-01-26 14:00:55 +0300

    Add cart page

--- src/app/components/main/main.controller.ts
+++ src/app/components/main/main.controller.ts
@@ -4,5 +4,12 @@ import { Utils } from "../../utils";
 export class MainController {
   public copyrightDate = new Date().getUTCFullYear();
 
-  public constructor(public shoppingCartService: IShoppingCartService, private utils: Utils) { }
+  public constructor(
+    public shoppingCartService: IShoppingCartService,
+    private utils: Utils,
+    private $state: angular.ui.IStateService, ) { }
+
+  public isCartState() {
+    return this.$state.is("main.cart");
+  }
 }
--- src/app/components/shopping-cart/shopping-cart.controller.ts
+++ src/app/components/shopping-cart/shopping-cart.controller.ts
@@ -1,6 +1,32 @@
-export class ShoppingCartController {
-  public test: string = "shopping cart";
-  constructor() {
+import { IShoppingCartService } from "../../lib/shopping-cart-service.interface";
+import { Utils } from "../../utils";
+import { IShoppingCartProduct } from "../../lib/shopping-cart-product.interface";
 
+export class ShoppingCartController implements angular.IController {
+  public products: IShoppingCartProduct[] = [];
+  constructor(public shoppingCartService: IShoppingCartService, private utils: Utils) {
+
+  }
+
+  public $onInit() {
+    this.products = this.shoppingCartService.getShoppingCartProducts();
+  }
+
+  public increase(product: IShoppingCartProduct) {
+    product.count++;
+    this.shoppingCartService.increase(product.product.id, 1);
+  }
+
+  public decrease(product: IShoppingCartProduct) {
+    if (product.count > 1) {
+      product.count--;
+      this.shoppingCartService.decrease(product.product.id, 1);
+    }
+  }
+
+  public remove(product: IShoppingCartProduct) {
+    this.shoppingCartService.remove(product.product.id);
+    let removeIndex = this.products.indexOf(product);
+    this.products.splice(removeIndex, 1);
   }
 }
